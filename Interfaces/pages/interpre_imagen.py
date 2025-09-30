2aa362c44cacf4bed7b9fe33de8a1aec8f2546da
Kr36pton <69650871+Kr36pton@users.noreply.github.com>
2025-09-29 20:15:59 -0500

    Create interpre_imagen.py

---
+++ Interfaces/pages/interpre_imagen.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+
+
+def app():
+st.header("Interpretación de Imágenes")
+st.markdown("Ingrese una imagen y obtenga una descripción detallada sobre la que se pueda preguntar. En desarrollo.")
