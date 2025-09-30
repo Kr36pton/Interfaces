ef3eabad1c47d277514f6c4c4174b6996d4e80d5
Kr36pton <69650871+Kr36pton@users.noreply.github.com>
2025-09-29 20:15:07 -0500

    Create ident_gestos.py

---
+++ Interfaces/pages/ident_gestos.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+
+
+def app():
+st.header("Identificación de Gestos")
+st.markdown("Detector en tiempo real con modelo exportado desde Teachable Machine y detector desde imágenes. En desarrollo.")
